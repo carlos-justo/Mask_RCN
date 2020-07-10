13de0b3ff0a4f4f9a7c038d1253fd883063e3d41
carlos-justo <33235171+carlos-justo@users.noreply.github.com>
2020-07-10 11:42:45 +0200

    Update visualize.py

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -81,7 +81,7 @@ def apply_mask(image, mask, color, alpha=0.5):
 
 
 def display_instances(image, boxes, masks, class_ids, class_names,
-                      scores=None, title="",
+                      image_save_path, scores=None, title="",
                       figsize=(16, 16), ax=None,
                       show_mask=True, show_bbox=True,
                       colors=None, captions=None):
@@ -163,6 +163,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
+    plt.savefig(image_save_path,bbox_inches='tight', pad_inches=-0.5,orientation= 'landscape')
     if auto_show:
         plt.show()
 
