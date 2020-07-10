5724272c9474c15a9d1c1935ebd64e9b3b65e9d6
carlos-justo <33235171+carlos-justo@users.noreply.github.com>
2020-07-10 12:55:47 +0200

    Update visualize.py

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -163,9 +163,10 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
-    plt.savefig(image_save_path,bbox_inches='tight',orientation= 'landscape')
+    #plt.savefig(image_save_path,bbox_inches='tight',orientation= 'landscape')
     if auto_show:
         plt.show()
+    return masked_image.astype(np.uint8)
 
 
 def display_differences(image,
